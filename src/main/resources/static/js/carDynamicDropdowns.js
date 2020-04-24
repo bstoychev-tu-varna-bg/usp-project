8e9dd9d21780bf6d810e8bb156d77cb1762b4654
BorislavStoychev <stoichev64@gmail.com>
2020-04-24 11:19:35 +0300

    Deleted unused code in carDynamicDropdown.js

--- src/main/resources/static/js/carDynamicDropdowns.js
+++ src/main/resources/static/js/carDynamicDropdowns.js
@@ -1,47 +1,4 @@
 $(function(){
-/*
-$("#brand-choice").change(function() {
-
-	var $dropdown = $(this);
-
-	$.getJSON("/jsondata/brandsAndModels.json", function(data) {
-
-		var key = $dropdown.val();
-		var vals = [];
-
-		switch(key) {
-			case 'Mercedes-Benz':
-				vals = data.mercedes-benz.split(",");
-				break;
-			case 'Audi':
-				vals = data.audi.split(",");
-				break;
-			case 'BMW':
-                vals = data.bmw.split(",");
-                break;
-            case 'Renault':
-                vals = data.renault.split(",");
-                break;
-            case 'Peugeot':
-                vals = data.peugeot.split(",");
-                break;
-            case 'Citroen':
-                vals = data.citroen.split(",");
-                break;
-			case 'base':
-				vals = ['Please choose from above'];
-		}
-
-		var $secondChoice = $("#model-choice");
-		$secondChoice.empty();
-		$.each(vals, function(index, value) {
-			$secondChoice.append("<option>" + value + "</option>");
-		});
-
-	});
-});
-*/
-
 $("#brand-choice").change(function() {
 
 	var $brandChoice = $(this);
